7cbdbef77d43635477b271327cb3513b43763c34
염태민 <onnu0802@naver.com>
2019-10-16 13:15:41 +0900

    vue-router 적용

--- src/router.js
+++ src/router.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import s1 from './views/s1.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'home',
-      component: Home
+      name: 's1',
+      component: s1
     },
 
   ]
